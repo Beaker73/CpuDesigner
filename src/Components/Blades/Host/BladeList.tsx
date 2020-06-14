57bec1969094f099c920020fe9617cf0fccb0465
Jeroen Smits <beaker73@live.nl>
2020-06-14 20:15:39 +0200

    scroll blade into view.

--- src/Components/Blades/Host/BladeList.tsx
+++ src/Components/Blades/Host/BladeList.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, Ref } from "react";
+import React, { useMemo, Ref, useRef, Children, useEffect } from "react";
 import { Stack, getTheme, mergeStyleSets, getParent } from "@fluentui/react";
 
 export interface BladeListProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -12,13 +12,21 @@ export function BladeList(props: React.PropsWithChildren<BladeListProps>): JSX.E
     const theme = getTheme();
     const style = useMemo(useStyle, [theme]);
 
+    // when number of children changes, scroll smoothly to end
+    const scrollRef = useRef<HTMLDivElement | null>(null);
+    const childCount = Children.count(props.children);
+    useEffect(() => {
+        scrollRef.current?.scrollIntoView({behavior: "smooth"});
+    }, [childCount]);
+
     return <Stack horizontal tokens={{ childrenGap: theme.spacing.m }} className={[props.className, style.bladeList].join(" ")}>
         {props.children}
         {/* 
             No margin at righ scroll side
             https://stackoverflow.com/questions/11695354/css-right-margin-does-not-work-inside-a-div-with-overflow-scroll 
+            as bonus: we now also have something to scroll into view when blade count changes
         */}
-        <div className={style.rightMargin} ></div>
+        <div className={style.rightMargin} ref={scrollRef}></div>
     </Stack>;
 
     function useStyle() {
